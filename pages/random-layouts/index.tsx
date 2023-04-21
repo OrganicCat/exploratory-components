38c963068c1adce28e8631abd234f4b454b3e618
Lee Delarm <lee.delarm@gmail.com>
2023-04-21 14:14:06 -0400

    Basic setup, bad colors

--- pages/random-layouts/index.tsx
+++ pages/random-layouts/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from './RandomLayouts.module.css'
 
 export default function RandomLayouts() {
     return (
@@ -9,8 +10,28 @@ export default function RandomLayouts() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            {/* Goals
+                - Add some nice colors and styles
+                - create some common pattern layouts
+                - make it responsive
+                - use flexbox and grid
+            */}
             <main>
                 <h1>Fun clean layouts, with style!</h1>
+
+                <div className={styles.centerMain}>
+                    <div className={styles.topSection}>
+                        <div className={styles.itemBox}>Top Section</div>
+                    </div>
+                    <div className={styles.middleSection}>
+                        <div className={`${styles.itemColumn} ${styles.itemBox}`}>Left Column</div>
+                        <div className={`${styles.itemColumn} ${styles.middleColumn}`}>Middle Column</div>
+                        <div className={`${styles.itemColumn} ${styles.itemBox}`}>Right Column</div>
+                    </div>
+                    <div className={styles.bottomSection}>
+                        <div className={styles.itemBox}>Bottom Section</div>
+                    </div>
+                </div>
             </main>
         </>
     )
